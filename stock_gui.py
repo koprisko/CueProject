2389388f403b5c66355ec85e99af492bec32f6c1
BrianHeckman <bh251971@muhlenberg.edu>
2019-04-23 15:46:24 -0400

    Update stock_gui.py

--- stock_gui.py
+++ stock_gui.py
@@ -21,7 +21,7 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 
 
-
+import numpy as np
 import time
 import calendar as cal
 import pandas as pd
@@ -65,39 +65,78 @@ class NeuralNetwork():
         # and mean 0.
         self.synaptic_weights = 2 * random.random((4, 1)) - 1
 
-    # The Sigmoid function, which describes an S shaped curve.
-    # We pass the weighted sum of the inputs through this function to
-    # normalise them between 0 and 1.
-    def __sigmoid(self, x):
-        return ((1 / (1 + exp(-x))))
+    # The Cost Function shows us how our model is performing. We chose MSE to show how far each point
+    # deviates from the actual point. The goal is to minimize this number.
+    def cost_function(self, features, targets):
 
-    # The derivative of the Sigmoid function.
-    # This is the gradient of the Sigmoid curve.
+        #Find length of list
+        N = len(targets)
+
+        predictions = dot(features, self.synaptic_weights)
+    
+        # Find square error for entire matrix
+        sq_error = (predictions - targets)**2
+
+        # Return average squared error among predictions
+        return 1.0/(2*N) * sq_error.sum()
+
+    # The derivative of the MSE cost function.
+    # This is the gradient of the MSE function that uses partial derivatives of each parameter.
     # It indicates how confident we are about the existing weight.
-    def __sigmoid_derivative(self, x):
-        return (x * (1 - x))
+    def update_weights(self,features, targets, lr):
 
-    # We train the neural network through a process of trial and error.
-    # Adjusting the synaptic weights each time.
-    def train(self, training_set_inputs, training_set_outputs, number_of_training_iterations):
+        # Find predictions using w1x1 + w2x2 + w3x3...concept 
+        predictions = dot(features, self.synaptic_weights)
+
+        #Extract our features
+        x1 = features[:,0]
+        x2 = features[:,1]
+        x3 = features[:,2]
+        x4 = features[:3]
+
+        """ 
+        print (x1)
+        print (x2)
+        print (x3)
+        print (x4)
+        """
+        # Use matrix cross product (*) to simultaneously
+        # calculate the derivative for each weight
+        d_w1 = -x1*(targets - predictions)
+        d_w2 = -x2*(targets - predictions)
+        d_w3 = -x3*(targets - predictions)
+        d_w4 = -x4*(targets - predictions)
+
+        # Multiply the mean derivative by the learning rate
+        # and subtract from our weights (remember gradient points in direction of steepest ASCENT)
+        self.synaptic_weights[0][0] -= (lr * np.mean(d_w1))
+        self.synaptic_weights[1][0] -= (lr * np.mean(d_w2))
+        self.synaptic_weights[2][0] -= (lr * np.mean(d_w3))
+        self.synaptic_weights[3][0] -= (lr * np.mean(d_w4))
+        
+        
+    def get_output(self,feature):
+        # Return predictions using w1x1 + w2x2 + w3x3... concept 
+        return dot(feature,self.synaptic_weights)
+
+   def train(self, training_set_inputs, training_set_outputs, number_of_training_iterations):
         for iteration in range(number_of_training_iterations):
             # Pass the training set through our neural network (a single neuron).
-            output = self.think(training_set_inputs)
-            # Calculate the error (The difference between the desired output
-            # and the predicted output).
-            error = training_set_outputs - output
-
-            # Multiply the error by the input and again by the gradient of the Sigmoid curve.
-            # This means less confident weights are adjusted more.
-            # This means inputs, which are zero, do not cause changes to the weights.
-            adjustment = dot(training_set_inputs.T, error * self.__sigmoid_derivative(output))
-            # Adjust the weights.
-            self.synaptic_weights += adjustment
+            output = self.think(training_set_inputs,training_set_outputs)
+            # Show the MSE of each iteration
+            print ("MSE:")
+            print (output)
+            # Update the weights by calling the function
+            self.update_weights(training_set_inputs,training_set_outputs,.8)
+            #Show new weights to see the change
+            print ("New weights:")
+            print (self.synaptic_weights)
 
     # The neural network thinks.
-    def think(self, inputs):
+    def think(self, inputs, outputs):
         # Pass inputs through our neural network (our single neuron).
-        return self.__sigmoid(dot(inputs, self.synaptic_weights))
+        return self.cost_function(inputs,outputs)
+    
 
 class Sentiment:
     def __init__(self, stock, ticker):
@@ -930,7 +969,7 @@ class Engine:
             actual = []
         
             for n in range(len(test_output)):
-                results.append(neural_network.think(array(test_input[n])))
+                results.append(neural_network.get_output(array(test_input[n])))
                 amount.append(n)
         
             results_regular = []
